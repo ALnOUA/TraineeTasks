be7da8964a38f1ecb84cc480af9b1b2c6c8bdd5f
AlexeNovokhatko <novohatko2000l@gmail.com>
2021-03-09 11:16:39 +0200

    Added Criteria

--- src/main/java/com/ua/services/CurrencyService.java
+++ src/main/java/com/ua/services/CurrencyService.java
@@ -5,6 +5,12 @@ import com.ua.repositories.CurrencyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 @Service
@@ -12,8 +18,16 @@ public class CurrencyService {
     @Autowired
     private CurrencyRepository repo;
 
+    @PersistenceContext
+    EntityManager entityManager;
     public List<Currency> listAll() {
-        return repo.findAll();
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Currency> criteriaQuery = criteriaBuilder.createQuery(Currency.class);
+        Root<Currency> currencyRoot = criteriaQuery.from(Currency.class);
+        criteriaQuery.select(currencyRoot);
+        TypedQuery<Currency> currencyTypedQuery = entityManager.createQuery(criteriaQuery);
+        List<Currency> currencies = currencyTypedQuery.getResultList();
+        return currencies;
     }
 
     public void save(Currency currency) {
